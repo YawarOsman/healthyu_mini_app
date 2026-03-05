788004a6a290e787a9e9cc22b27ab66083c0ed5f
YawarOsman <yawarosmankhdir@gmail.com>
2026-03-05 16:32:24 +0300

    cleaning route mismatches

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -7,15 +7,12 @@ const MINI_ROUTES = {
   REGISTER_NAME_DOB: toMiniRoute('pages/register/name_dob/index'),
   REGISTER_SETUP_ACCOUNT: toMiniRoute('pages/register/setup_account/index'),
   REGISTER_OTP_VERIFICATION: toMiniRoute('pages/register/otp_verification/index'),
-  CAMERA: toMiniRoute('pages/camera/index'),
   ORDER: toMiniRoute('pages/order/index'),
   ORDER_SHIPPING: toMiniRoute('pages/order_shipping/index'),
   SCAN_BOX: toMiniRoute('pages/scan_box/index'),
   BOXES: toMiniRoute('pages/boxes/index'),
-  ANSWERS: toMiniRoute('pages/answers/index'),
   DISCOVER: toMiniRoute('pages/discover/index'),
   ME: toMiniRoute('pages/me/index'),
-  BOX_REGISTRATION: toMiniRoute('pages/scan_box/index'),
 } as const
 
 export const ROUTES = MINI_ROUTES
